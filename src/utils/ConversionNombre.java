a92e05a2df839fd1a4ee68a5890cfa6568f55a73
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-06-10 17:25:38 +0200

    Realisation du TP4

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,21 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		
+		String nbr = "100";
+		int result = Integer.parseInt(nbr);
+		System.out.println("Le résultat est : " + result);
+
+		int a = 15;
+		int b = 23;
+		
+		int max = Integer.max(a, b);	
+		
+		System.out.println(max);
+		
+
+	}
+
+}
