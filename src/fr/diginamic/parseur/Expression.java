b3ee1269ce5781cd19385a4850d3682fddf6d717
sanaeltahhan <sana.eltahhan@hotmail.fr>
2020-06-16 01:08:29 +0200

    TP-J3 - Anto-formation : Debut  Exercice Parseur(facultatif)

---
+++ src/fr/diginamic/parseur/Expression.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package fr.diginamic.parseur;
+
+/**
+ * @author eltahhansana
+ *
+ */
+public class Expression {
+	
+	public void evaluer(String var, double valVar) {
+		
+	}
+
+}
---
+++ src/fr/diginamic/parseur/Parseur.java
@@ -0,0 +1,9 @@
+package fr.diginamic.parseur;
+
+public class Parseur {
+	
+	private String gauche;
+	private char operateur;
+	private String droite;
+
+}
